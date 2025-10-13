42597735826e0a5e61d30a744c39ed8ed7c97669
rouuhehe <rojasevelyn88@gmail.com>
2025-10-12 21:24:41 -0500

    refactor(design): split half and single precision alignment logic into separate modules

--- src/design/single/fp32_align.v
+++ src/design/single/fp32_align.v
@@ -1,29 +1,62 @@
 `timescale 1ns / 1ns
 
-module fp_align(
-        output reg [22:0] aligned_a, // new mantissa 
-        output reg [22:0] aligned_b, // new mantissa 
-        output reg [7:0] exp,
+module fp32_align(
+        output reg [22:0] OUT_MANT_A_SINGLE, // new mantissa 
+        output reg [22:0] OUT_MANT_B_SINGLE, // new mantissa 
+        output reg [7:0] OUT_EXP_SINGLE,
+        output reg STICKY_BIT,
+        output reg EXCEPTION_ALIGN_SINGLE,
 
-        input wire [7:0] EXP_A,
-        input wire [7:0] EXP_B,
-        input wire [22:0] MANT_A,
-        input wire [22:0] MANT_B
+        input wire [7:0] IN_EXP_A_SINGLE,
+        input wire [7:0] IN_EXP_B_SINGLE,
+        input wire [22:0] IN_MANT_A_SINGLE,
+        input wire [22:0] IN_MANT_B_SINGLE
     );
 
+    
     reg [7:0] sub;
+
     always @ (*) begin
-        if(EXP_A < EXP_B) begin 
-            sub = EXP_B - EXP_A;
-            aligned_a = MANT_A >> sub;
-            aligned_b = MANT_B;
-            exp =  EXP_B;
+
+        OUT_MANT_A_SINGLE = IN_MANT_A_SINGLE;
+        OUT_MANT_B_SINGLE = IN_MANT_B_SINGLE;
+        OUT_EXP_SINGLE = IN_EXP_A_SINGLE;
+        EXCEPTION_ALIGN_SINGLE = 0;
+        STICKY_BIT = 0;
+
+        // Exception flag sets 1 if either one of the exponents is 31.
+        if(( &IN_EXP_A_SINGLE ) | ( &IN_EXP_B_SINGLE )) begin
+            EXCEPTION_ALIGN_SINGLE = 1;
+            OUT_MANT_A_SINGLE = 0;
+            OUT_MANT_B_SINGLE = 0;
+            OUT_EXP_SINGLE = 8'd255;
         end
-        else begin 
-            sub = EXP_A - EXP_B;
-            aligned_b = MANT_B >> sub;
-            aligned_a = MANT_A;
-            exp = EXP_A;
+        else if (IN_EXP_A_SINGLE > IN_EXP_B_SINGLE) begin
+            sub = IN_EXP_A_SINGLE - IN_EXP_B_SINGLE;
+            OUT_EXP_SINGLE = IN_EXP_A_SINGLE;
+
+            if(sum >= 23) begin // if one exp is much bigger than the other
+                OUT_MANT_B_SINGLE = 0;
+                STICKY_BIT  = |IN_MANT_B_SINGLE;
+            end
+            else begin
+                OUT_MANT_B_SINGLE = IN_MANT_B_SINGLE >> sub;
+                STICKY_BIT = |(IN_MANT_B_SINGLE[sub-1:0]);    
+            end
+        end 
+        
+        else if (IN_EXP_B_SINGLE > IN_EXP_A_SINGLE) begin
+            sub = IN_EXP_A_SINGLE - IN_EXP_B_SINGLE;
+            OUT_EXP_SINGLE = IN_EXP_A_SINGLE;
+
+            if(sum >= 23) begin
+                OUT_MANT_A_SINGLE = 0;
+                STICKY_BIT  = |IN_MANT_A_SINGLE;
+            end
+            else begin
+                OUT_MANT_A_SINGLE = IN_MANT_A_SINGLE >> sub;
+                STICKY_BIT = |(IN_MANT_A_SINGLE[sub-1:0]);    
+            end
         end
     end
 
--- src/design/single/fp32_decode.v
+++ src/design/single/fp32_decode.v
@@ -1,63 +1,24 @@
 `timescale 1ns / 1ns
 
-module fp32_align(
-        output reg [22:0] OUT_MANT_A_SINGLE, // new mantissa 
-        output reg [22:0] OUT_MANT_B_SINGLE, // new mantissa 
-        output reg [7:0] OUT_EXP_SINGLE,
-        output reg STICKY_BIT,
-        output reg EXCEPTION_ALIGN_SINGLE,
+module fp32_decode(
+    output wire SIGN_A_SINGLE,
+    output wire SIGN_B_SINGLE,
+    output wire [7:0] EXP_A_SINGLE,
+    output wire [7:0] EXP_B_SINGLE,
+    output wire [22:0] MANT_A_SINGLE,
+    output wire [22:0] MANT_B_SINGLE,
 
-        input wire [7:0] IN_EXP_A_SINGLE,
-        input wire [7:0] IN_EXP_B_SINGLE,
-        input wire [22:0] IN_MANT_A_SINGLE,
-        input wire [22:0] IN_MANT_B_SINGLE
+    input [31:0] OP_A_SINGLE,
+    input [31:0] OP_B_SINGLE,
     );
 
-    
-    reg [7:0] sub;
+    assign SIGN_A_SINGLE = OP_A_SINGLE[31];
+    assign SIGN_B_SINGLE = OP_B_SINGLE[31];
 
-    always @ (*) begin
+    assign [7:0] EXP_A_SINGLE = OP_A_SINGLE[30:23];
+    assign [7:0] EXP_B_SINGLE = OP_B_SINGLE[30:23];
 
-        OUT_MANT_A_SINGLE = IN_MANT_A_SINGLE;
-        OUT_MANT_B_SINGLE = IN_MANT_B_SINGLE;
-        OUT_EXP_SINGLE = IN_EXP_A_SINGLE;
-        EXCEPTION_ALIGN_SINGLE = 0;
-        STICKY_BIT = 0;
-
-        // Exception flag sets 1 if either one of the exponents is 31.
-        if(( &IN_EXP_A_SINGLE ) | ( &IN_EXP_B_SINGLE )) begin
-            EXCEPTION_ALIGN_SINGLE = 1;
-            OUT_MANT_A_SINGLE = 0;
-            OUT_MANT_B_SINGLE = 0;
-            OUT_EXP_SINGLE = 8'd255;
-        end
-        else if (IN_EXP_A_SINGLE > IN_EXP_B_SINGLE) begin
-            sub = IN_EXP_A_SINGLE - IN_EXP_B_SINGLE;
-            OUT_EXP_SINGLE = IN_EXP_A_SINGLE;
-
-            if(sum >= 23) begin // if one exp is much bigger than the other
-                OUT_MANT_B_SINGLE = 0;
-                STICKY_BIT  = |IN_MANT_B_SINGLE;
-            end
-            else begin
-                OUT_MANT_B_SINGLE = IN_MANT_B_SINGLE >> sub;
-                STICKY_BIT = |(IN_MANT_B_SINGLE[sub-1:0]);    
-            end
-        end 
-        
-        else if (IN_EXP_B_SINGLE > IN_EXP_A_SINGLE) begin
-            sub = IN_EXP_A_SINGLE - IN_EXP_B_SINGLE;
-            OUT_EXP_SINGLE = IN_EXP_A_SINGLE;
-
-            if(sum >= 23) begin
-                OUT_MANT_A_SINGLE = 0;
-                STICKY_BIT  = |IN_MANT_A_SINGLE;
-            end
-            else begin
-                OUT_MANT_A_SINGLE = IN_MANT_A_SINGLE >> sub;
-                STICKY_BIT = |(IN_MANT_A_SINGLE[sub-1:0]);    
-            end
-        end
-    end
+    assign [22:0] MANT_A_SINGLE = OP_A_SINGLE[22:0];
+    assign [22:0] MANT_B_SINGLE = OP_B_SINGLE[22:0];
 
 endmodule
