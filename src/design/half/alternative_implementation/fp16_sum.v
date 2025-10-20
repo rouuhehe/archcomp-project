8fc575837223b137967fac7caac7a784662b7187
rouuhehe <yaritza.lopez@utec.edu.pe>
2025-10-20 10:44:16 -0500

    remove unused modules

--- src/design/half/alternative_implementation/fp16_sum.v
+++
@@ -1,7 +0,0 @@
-module fp16_sum(
-  output [14:0] OUT_MANT_SUM,
-  input [13:0] IN_MANT_A_HALF_EXT,
-  input [13:0] IN_MANT_B_HALF_EXT
-);
-  assign OUT_MANT_SUM = IN_MANT_A_HALF_EXT + IN_MANT_B_HALF_EXT;
-endmodule
